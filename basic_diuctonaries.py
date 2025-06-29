160a9aaa9bc247b0ca90f4ea9177f885d27535c1
MD RASHID ALI HAIDER <rashidnrlbhilai@gmail.com>
2025-06-29 23:45:50 +0530

    Create basic_dictonaries.py

---
+++ basic_diuctonaries.py
@@ -0,0 +1,11 @@
+student = {
+    "name" : "Rashid",
+    "id" : 12345,
+    "age" : 20, 
+    "courses": {
+        "math" : 90,
+        "english" : 85,
+        "science" : 88
+    }
+}
+print(student)  
