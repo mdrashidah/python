3c6d07b7f28dec892f52cf9cd4a7a178ff48fb22
MD RASHID ALI HAIDER <rashidnrlbhilai@gmail.com>
2025-07-13 18:52:06 +0530

    Update multiplication_table_generator.py

--- multiplication_table_generator.py
+++ multiplication_table_generator.py
@@ -4,3 +4,11 @@ print("This program generates a multiplication table for a given number from 0 t
 num = int(input("Enter a multiplier number: "))
 for i in range(0, 11):
     print(f"{num} * {i} = {num * i}")
+
+
+print("This program generates a reverse multiplication table for a given number from 0 to 10.")
+# Get the reverse multiplier number from the user
+num = int(input("Enter a multiplicant number: "))
+for i in range(10,-1,-1):
+    print(f"{num} * {i} = {num * i}")
+
